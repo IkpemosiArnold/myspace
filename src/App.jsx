dfebbdc88e4da5fbca31f7b2587beb36358dbe40
Ikpemosi Arnold <Ikpemosi@protonmail.com>
2023-02-17 10:30:11 +0100

    "first commit"

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I'm Ikpemosi Arnold.</h1>
+      <p>On most days I believe there's nothing I can't do.</p>
+    </div>
+  );
+}
+
+export default App;
