2cc4a6a19199e429b8de09625c1311c352758735
Ikpemosi Arnold <Ikpemosi@protonmail.com>
2023-08-09 03:16:22 +0100

    Favicon, Open to work + minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import jetAge from "../assets/JetAgeSolutions.png";
 import theosis from "../assets/TheosisInvite.png";
 import lvmedia from "../assets/LVMEDIA.png";
+import magnuminsight from "../assets/MangnumInsight.png";
+import fatmin from "../assets/Fatmin.png";
 
 const Home = () => {
+  const [today, setToday] = useState("");
+  useEffect(() => {
+    const dayOfWeekName = new Date().toLocaleString("default", {
+      weekday: "long",
+    });
+    setToday(dayOfWeekName);
+  }, []);
   return (
     <div className="App">
       <Container>
-        <h2>
-          Hi, I'm Ikpemosi.{" "}
+        <nav className="navbar">
+          <h2>Ikpemosi Arnold</h2>
+
+          <p className="work-status">
+            <span className="green-glow"> </span>Open to work
+          </p>
+
+          <a href="mailto:IkpemosiArnold@outlook.com">Send me an email</a>
+        </nav>
+        <h1>
+          Software Engineer <br></br>
           <span className="subheading">
             On most days I believe there's nothing I can't do.
           </span>
-        </h2>
+        </h1>
 
         <p className="profile-summary">
-          Currently, I write code and help businesses scale their operations by
-          contributing in different capacities; strategy, stakeholder management
-          etc., all termed under "Product Management". Previously, I worked as a
-          Projects and Programs Manager at a consulting firm focused on Climate
-          Change and Sustainable Development.
+          Currently, <span className="highlight">I write code</span> and help
+          businesses scale their operations by figuring out what their customers
+          really need and making sure the product meets these needs seamlessly.
         </p>
         <p className="profile-summary">
           {" "}
@@ -34,21 +50,14 @@ const Home = () => {
           </span>
           .
         </p>
-        <h3>Recent Work</h3>
+        <h3>Toolkit</h3>
+        <span className="subheading">Languages, Platforms and tools</span>
+        <p className="profile-summary">
+          Svelte, NodeJs, React, HTML, JavaScript, TypeScript, Python, Framer,
+          Wordpress, Webflow{" "}
+        </p>
+        <h3>Projects</h3>
         <div className="work-section">
-          <div className="work-boxes">
-            <a href="http://jetage.org/">
-              {" "}
-              <img
-                className="work-image"
-                src={jetAge}
-                alt="screenshot of jetage website"
-                loading="lazy"
-              />
-            </a>
-            <p>WP, Design</p>
-          </div>
-
           <div className="work-boxes">
             <a href="https://lvmedia.ng/">
               <img
@@ -58,7 +67,26 @@ const Home = () => {
                 loading="lazy"
               />
             </a>
-            <p>ReactJs, NodeJs, MongoDB</p>
+            <p className="work-desc">
+              Frontend and Backend for LVmedia.ng, B2C platform that allows
+              users to purchase social media services<br></br>
+              <span className="subheading">ReactJs, NodeJs, MongoDB</span>
+            </p>
+          </div>
+          <div className="work-boxes">
+            <a href="https://magnuminsight.com">
+              <img
+                className="work-image"
+                src={magnuminsight}
+                alt="screenshot of MagnumInsight Login page"
+                loading="lazy"
+              />
+            </a>
+            <p className="work-desc">
+              Frontend for MagnumInsight, a Canadian Immigration Consulting
+              Company. Focus especially on User Dashboard<br></br>
+              <span className="subheading">ReactJs, NodeJs</span>
+            </p>
           </div>
           <div className="work-boxes">
             <a href="https://theoverse.netlify.app/">
@@ -69,9 +97,28 @@ const Home = () => {
                 loading="lazy"
               />
             </a>
-            <p>React, NodeJs</p>
+            <p className="work-desc">
+              Frontend and Backend for Theosis, An AI powered game <br></br>
+              <span className="subheading">Svelte, Python, NodeJs</span>
+            </p>
+          </div>
+          <div className="work-boxes">
+            <a href="#">
+              {" "}
+              <img
+                className="work-image"
+                src={jetAge}
+                alt="screenshot of jetage website"
+                loading="lazy"
+              />
+            </a>
+            <p className="work-desc">
+              Website for Jetage, Renewable Energy Consulting Company <br></br>
+              <span className="subheading">Wordpress</span>
+            </p>
           </div>
         </div>
+        <footer>Have a wonderful {today} 🤗</footer>
       </Container>
     </div>
   );
